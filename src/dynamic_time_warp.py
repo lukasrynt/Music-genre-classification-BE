0ed4e53a3b25a162398a48cc12e52df4dfc1ccc3
Lukáš Rynt <ryntluka@fit.cvut.cz>
2022-11-23 09:31:09 +0100

    Fix dynamic time warping and finish testing

--- src/dynamic_time_warp.py
+++ src/dynamic_time_warp.py
@@ -8,17 +8,18 @@ def dtw(first, second, window: int = 10, distance=euclidean):
     m = len(second)
     window = max(window, abs(n - m))
     matrix = np.full((n + 1, m + 1), np.inf)
+    matrix[0, 0] = 0
 
-    for i in range(n + 1):
+    for i in range(1, n + 1):
         for j in range(max(1, i - window), min(m, i + window) + 1):
             matrix[i, j] = 0
 
-    for i in range(n + 1):
+    for i in range(1, n + 1):
         for j in range(max(1, i - window), min(m, i + window) + 1):
             curr_i = i - 1
             curr_j = j - 1
             cost = distance(first[curr_i], second[curr_j])
-            matrix[i, j] = cost + min(matrix[curr_i - 1, curr_j],  # insertion
-                                      matrix[curr_i, curr_j - 1],  # deletion
-                                      matrix[curr_i - 1, curr_j - 1])  # match
+            matrix[i, j] = cost + min(matrix[curr_i, j],
+                                      matrix[i, curr_j],
+                                      matrix[curr_i, curr_j])
     return matrix[n, m]
